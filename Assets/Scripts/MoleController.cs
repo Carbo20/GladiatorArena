0eccd7a919d05221eae7c2b6f55fbaaa9ca0fd4e
David YANA <david.yana@yahoo.fr>
2015-10-08 22:39:22 +0200

    Deplacement avec l'accelerometre

---
+++ Assets/Scripts/MoleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleController : MonoBehaviour {
+
+    private Vector2 direction;
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+
+	}
+
+    private void Move()
+    {
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+        dir.z = Input.acceleration.y;
+        /* if (dir.sqrMagnitude > 1)
+             dir.Normalize();*/
+
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+    }
+
+    public void OnGUI()
+    {
+        GUI.TextArea(new Rect(5, 5, 120, 20), Input.acceleration.ToString());
+    }
+}
