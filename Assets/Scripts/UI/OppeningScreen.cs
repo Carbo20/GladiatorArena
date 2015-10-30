f789986fd22f385eb637d74cfc3b6bb2a27c0e10
Carbo <carbo20@hotmail.fr>
2015-10-30 13:50:13 +0100

    add lava stuff

---
+++ Assets/Scripts/UI/OppeningScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OppeningScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+
+
+	}
+
+    public void SwitchToPlayScene()
+    {
+        //Put The play scene here
+    }
+
+    public void SwitchToOptionScene()
+    {
+        //Put the option scene here
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
